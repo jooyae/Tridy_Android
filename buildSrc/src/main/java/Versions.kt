60e7ba5f86f2ab8883369b6ba232b3e6caa7806b
sonpyeonghwa <ssrsph@naver.com>
2021-08-25 12:23:03 +0900

    chore: gradle kotlin.dsl 적용

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,33 +1,66 @@
-object Versions {
-    const val kotlin_version = "1.5.0"
+object DependencyVersions {
+    const val KOTLIN_VERSION = "1.5.21"
+    const val GRADLE_VERSION = "7.0.1"
 
-    const val retrofit_version = "2.9.0"
-    const val okhttp_version = "4.9.0"
+    //AndroidX
+    const val CORE_KTX_VERSION = "1.6.0"
+    const val APPCOMPAT_VERSION = "1.3.1"
+    const val MATERIAL_VERSION = "1.4.0"
+    const val CONSTRAINT_LAYOUT_VERSION = "2.1.0"
+    const val FRAGMENT_KTX_VERSION = "1.3.4"
+    const val ACTIVITY_KTX_VERSION = "1.2.3"
 
-    const val rxjava_version = "2.1.9"
-    const val rxandroid_version = "2.0.2"
+    //SwipeRefreshLayout
+    const val SWIPE_REFRESH_LAYOUT_VERSION = "1.1.0"
 
-    const val room_version = "2.3.0-alpha04"
-    const val dagger_version = "2.35.1"
-    const val lorem_version = "2.1"
+    //Crypto
+    const val CRYPTO_VERSION = "1.0.0"
 
-    const val paging_version = "3.0.0"
+    //Navigation
+    const val NAVIGATION_VERSION = "2.3.5"
 
-    const val glide_version = "4.12.0"
-    const val coil_version = "1.1.1"
+    //Room
+    const val ROOM_VERSION = "2.4.0-alpha03"
 
-    const val navigation_version = "2.3.5"
-    const val fragment_ktx_version = "1.3.4"
-    const val activity_ktx_version = "1.2.3"
-    const val lifecycle_version = "2.3.1"
+    //Lifecycle
+    const val LIFECYCLE_VERSION = "2.3.1"
 
-    const val annotations_version = "28.0.0"
+    //Hilt
+    const val DAGGER_HILT_VERSION = "2.37"
 
-    const val crypto_version = "1.0.0"
+    //Retrofit
+    const val RETROFIT_VERSION = "2.9.0"
+    const val OKHTTP_VERSION = "4.9.0"
 
-    const val swipe_refresh_layout_version = "1.1.0"
+    //Gson
+    const val GSON_VERSION = "2.8.6"
 
+    //RxJava
+    const val RXJAVA_VERSION = "2.1.9"
+    const val RXANDROID_VERSION = "2.0.2"
+
+    //Coil
+    const val COIL_VERSION = "1.1.1"
+
+    //Annotations
+    const val ANNOTATION_VERSION = "28.0.0"
+
+    //Kakao
     const val KAKAO_SDK_USER_VERSION = "2.6.0"
-
     const val KAKAO_USERMGMT_VERSION = "1.27.0"
+
+    //Firebase
+    const val PLAY_SERVICE_AUTH = "19.2.0"
+    const val GOOGLE_SERVICES = "4.3.10"
+    const val FIREBASE_BOM = "28.3.1"
+
+    const val LOREM_VERSION = "2.1"
+    const val PAGING_VERSION = "3.0.0"
+    const val GLIDE_VERSION = "4.12.0"
+}
+
+object TestVersions {
+    const val JUNIT = "4.13.2"
+    const val EXT_JUNIT = "1.1.3"
+    const val ESPRESSO_VERSION = "3.4.0"
 }
